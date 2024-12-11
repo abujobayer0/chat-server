6339df543f04b9ce68a3c3ca5175e0103f136c00
abu jobayer <zubayer.munna.dev@gmail.com>
2024-12-12 01:11:10 +0600

    cors fixed

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-app-mauve-beta.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-app-mauve-beta.vercel.app',
     methods: ['GET', 'POST'],
   })
 );
